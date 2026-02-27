37c7491ed1b12de929ef79e64072aa0c41a2927b
ramchandravgr <ramchandra.vgr@gmail.com>
2026-02-27 07:28:02 +0530

    Add assignment files for loops and operators

---
+++ Assignments/ForLoop.js
@@ -0,0 +1,22 @@
+for (i = 0; i < 5; i++)
+    console.log("Hello");
+
+for (i = 1; i <= 10; i++)
+    console.log(i);
+
+for (i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+let a = 0;
+for (i = 1; i <= 10; i++) {
+    a = a + i
+}
+console.log(a);
+
+for (i = 1; i <= 10; i++) {
+    console.log(5 + "*" + i + "=" + 5 * i)
+}
+
---
+++ Assignments/WhileLoop.js
@@ -0,0 +1,33 @@
+let i = 0;
+while (i < 5) {
+    console.log("Playwright");
+    i++;
+}
+
+let p = 1;
+while (p <= 10) {
+    console.log(p);
+    p++;
+}
+
+let q = 1;
+while (q <= 20) {
+    if (q % 2 === 0) {
+        console.log(q)
+    }
+    q++;
+}
+
+let r = 1;
+let t = 0;
+while (r <= 10) {
+    t = t + r;
+    r++;
+}
+console.log(t);
+
+let s = 1;
+while (s <= 10) {
+    console.log(7 + "*" + s + "=" + 7 * s)
+    s++;
+}
